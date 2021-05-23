943785bbaedfd040fc0bd97f5f307321e08dc011
Kosmik <malpa.w.klapkach@gmail.com>
2021-05-23 23:18:48 +0200

    Removal of unnecessary namespaces

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine;
-using UnityEngine.UIElements;
+﻿using UnityEngine;
 
 public class SunMovement : MonoBehaviour
 {
