534c2e1f8de98b171d6eb2b7c8e4bd3fa2ae1b0a
Kosmik <malpa.w.klapkach@gmail.com>
2025-03-21 14:38:01 +0100

    Add sample assets

--- Scripts/TimeSettings.cs
+++ Scripts/TimeSettings.cs
@@ -6,14 +6,8 @@ namespace RealisticSunMovement
 	public class TimeSettings : ScriptableObject
 	{
 		[field: SerializeField]
-		public float SecondsInMinute { get; set; } = 60;
+		public float SecondsInDay { get; set; } = 60 * 60 * 24;
 
-		[field: SerializeField]
-		public float MinutesInHour { get; set; } = 60;
-
-		[field: SerializeField]
-		public float HoursInDay { get; set; } = 24;		
-		
 		[field: SerializeField]
 		public float DaysInYear { get; set; } = 365.242199f;
 	}
