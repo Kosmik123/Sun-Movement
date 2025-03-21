c245721ba1dbc74cc35c656882021dc2ec6f5e12
Kosmik <malpa.w.klapkach@gmail.com>
2025-03-21 14:56:13 +0100

    Add time updating to the system

--- Scripts/Clock.cs
+++ Scripts/Clock.cs
@@ -4,7 +4,19 @@ namespace RealisticSunMovement
 {
 	[System.Serializable]
 	public class Clock
-    {
+	{
+		[SerializeField, Range(0, 1)]
+		[Tooltip("Midnight: 0 and 1\nNoon: 0.5")]
+		private float dayProgress = 0.5f;
+		public float DayProgress
+		{
+			get => dayProgress;
+			set
+			{
+				dayProgress = Mathf.Clamp01(value);
+			}
+		}
+
 		[SerializeField, Range(0, 1)]
         [Tooltip("Winter solstice: 0 and 1\nSpring equinox: 0.25\nSummer solstice: 0.5\nAutumn equinox: 0.75\n")]
 		private float yearProgress = 0.5f;
@@ -14,18 +26,6 @@ namespace RealisticSunMovement
 			set
 			{
 				yearProgress = Mathf.Clamp01(value);
-			}
-		}
-
-		[SerializeField, Range(0, 1)]
-        [Tooltip("Midnight: 0 and 1\nNoon: 0.5")]
-		private float dayProgress = 0.5f;
-		public float DayProgress
-		{
-			get => dayProgress;
-			set
-			{
-                dayProgress = Mathf.Clamp01(value);
 			}
 		}
     }
--- Scripts/TimeUpdater.cs
+++ Scripts/TimeUpdater.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace RealisticSunMovement
 {
@@ -30,13 +31,60 @@ namespace RealisticSunMovement
 		[SerializeField]
 		private SunMovement sunMovement;
 
+		[Header("States")]
+		[SerializeField]
 		private float rawSeconds;
+		[SerializeField]
 		private float dayCount;
+
+		private void OnEnable()
+		{
+			float yearProgress = sunMovement.Clock.YearProgress;
+			float dayProgress = sunMovement.Clock.DayProgress;
+
+			rawSeconds = dayProgress * timeSettings.SecondsInDay;
+			dayCount = yearProgress * timeSettings.DaysInYear;
+		}
 
 		private void Update()
 		{
-			float secondsInDay = timeSettings.SecondsInDay;
 			rawSeconds += Time.deltaTime * timeSpeed;
+			ValidateRawValues();
+			UpdateSunClock();
+		}
+
+		private void ValidateRawValues()
+		{
+			float daysInYear = timeSettings.DaysInYear;
+			float secondsInDay = timeSettings.SecondsInDay;
+			if (rawSeconds >= secondsInDay || rawSeconds < 0)
+			{
+				int fullDays = Mathf.FloorToInt(rawSeconds / secondsInDay);
+				float secodsRemaining = rawSeconds - fullDays * secondsInDay;
+				dayCount += fullDays;
+				rawSeconds = secodsRemaining;
+			}
+
+			if (dayCount < 0 || dayCount >= daysInYear)
+			{
+				int fullYears = Mathf.FloorToInt(dayCount / daysInYear);
+				float daysRemaining = dayCount - fullYears * daysInYear;
+				dayCount = daysRemaining;
+			}
+		}
+
+		private void UpdateSunClock()
+		{
+			float yearProgress = dayCount / timeSettings.DaysInYear;
+			float dayProgress = rawSeconds / timeSettings.SecondsInDay;
+
+			sunMovement.Clock.YearProgress = yearProgress;
+			sunMovement.Clock.DayProgress = dayProgress;
+		}
+
+		private void OnValidate()
+		{
+			ValidateRawValues();
 		}
 	}
 }
